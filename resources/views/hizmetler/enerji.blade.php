dea8c1c072588c2ced00d06dd376b760efe3599e
Metin SARITAS <metinsaritas2@gmail.com>
2018-07-23 17:54:49 +0300

    icons: bilisim enerji otomasyon

--- resources/views/hizmetler/enerji.blade.php
+++ resources/views/hizmetler/enerji.blade.php
@@ -7,6 +7,75 @@
         <span class="title">Enerji Sistemleri</span>
     </div>
 </div>
+
+
 @section("icerik")
-asdasd
+<div>
+    <div class="hizmet-after">
+        <div class="cont">
+        <div class="container">
+            <div class="row hizmet-menu">
+                <div data-to="kojenerasyon" class="col-sm list active" style="background-image:url(../img/hizmetler/tub.png)">
+                    <span>Kojenerasyon Sistemleri</span>
+                    <div class="arrow"></div>
+                </div>
+                
+                <div data-to="solar" class="col-sm list" style="background-image:url(../img/hizmetler/sun.png)">
+                    <span>Solar Enerji Sistemleri</span>
+                    <div class="arrow"></div>
+                </div>
+                <div data-to="ruzgar" class="col-sm list" style="background-image:url(../img/hizmetler/turbine.png)">
+                    <span>Rüzgar Enerjisi</span>
+                    <div class="arrow"></div>
+                </div>
+                <div data-to="karbon" class="col-sm list" style="background-image:url(../img/hizmetler/c.png)">
+                    <span>Karbon Üretim Tesisi</span>
+                    <div class="arrow"></div>
+                </div>
+                <div data-to="bio" class="col-sm list" style="background-image:url(../img/hizmetler/human.png)">
+                    <span>Bio Enerji Sistemleri</span>
+                    <div class="arrow"></div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+
+    <div class="container-area">
+        <div class="container">
+            <div data-name="kojenerasyon" class="tab active">
+                Burası kojenerasyon hizmetler kısmı
+            </div>
+
+            <div data-name="solar" class="tab">
+                Burası solar hizmetler kısmı
+            </div>
+
+            <div data-name="ruzgar" class="tab">
+                Burası ruzgar hizmetler kısmı
+            </div>
+
+            <div data-name="karbon" class="tab">
+                Burası karbon hizmetler kısmı
+            </div>
+
+            <div data-name="bio" class="tab">
+                Burası bio hizmetler kısmı
+            </div>                
+        </div>
+    </div>
+</div>
+<script src="{{ asset('vendor/jquery/jquery.js')}}"></script>
+<script>
+    $(document).on("click", ".hizmet-menu .list[data-to]:not(.active)", function() {
+        $(".hizmet-menu .list.active[data-to]").removeClass("active");
+        $(this).addClass("active");
+        let to = $(this).data("to");
+        $(".container-area .tab").hide();
+        $(".container-area .tab").filter("[data-name='"+to+"']").show();
+
+        let s = $("body").width() > 768 ?  280 : 220;
+        $("body").animate({scrollTop: s}, 300);
+    });
+</script>
 @endsection
--- resources/views/hizmetler/otomasyon.blade.php
+++ resources/views/hizmetler/otomasyon.blade.php
@@ -9,5 +9,72 @@
 </div>
 
 @section("icerik")
-Merhaba
+<div>
+    <div class="hizmet-after">
+        <div class="cont">
+        <div class="container">
+            <div class="row hizmet-menu">
+                <div data-to="atikizleme" class="col-sm list active" style="background-image:url(../img/hizmetler/trash.png)">
+                    <span>Akıllı Atık İzleme Sistemleri</span>
+                    <div class="arrow"></div>
+                </div>
+                
+                <div data-to="atiktoplama" class="col-sm list" style="background-image:url(../img/hizmetler/track.png)">
+                    <span>Akıllı Atık Toplama Sistemleri</span>
+                    <div class="arrow"></div>
+                </div>
+                <div data-to="sehirler" class="col-sm list" style="background-image:url(../img/hizmetler/home.png)">
+                    <span>Akıllı Şehirler</span>
+                    <div class="arrow"></div>
+                </div>
+                <div data-to="aydinlatma" class="col-sm list" style="background-image:url(../img/hizmetler/lamp.png)">
+                    <span>Akıllı Aydınlatma Sistemleri</span>
+                    <div class="arrow"></div>
+                </div>
+                <div data-to="elektrik" class="col-sm list" style="background-image:url(../img/hizmetler/battery.png)">
+                    <span>Elektrik Şarj Üniteleri</span>
+                    <div class="arrow"></div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+
+    <div class="container-area">
+        <div class="container">
+            <div data-name="atikizleme" class="tab active">
+                Burası atikizleme hizmetler kısmı
+            </div>
+
+            <div data-name="atiktoplama" class="tab">
+                Burası atiktoplama hizmetler kısmı
+            </div>
+
+            <div data-name="sehirler" class="tab">
+                Burası sehirler hizmetler kısmı
+            </div>
+
+            <div data-name="aydinlatma" class="tab">
+                Burası aydinlatma hizmetler kısmı
+            </div>
+
+            <div data-name="elektrik" class="tab">
+                Burası elektrik hizmetler kısmı
+            </div>
+        </div>
+    </div>
+</div>
+<script src="{{ asset('vendor/jquery/jquery.js')}}"></script>
+<script>
+    $(document).on("click", ".hizmet-menu .list[data-to]:not(.active)", function() {
+        $(".hizmet-menu .list.active[data-to]").removeClass("active");
+        $(this).addClass("active");
+        let to = $(this).data("to");
+        $(".container-area .tab").hide();
+        $(".container-area .tab").filter("[data-name='"+to+"']").show();
+
+        let s = $("body").width() > 768 ?  280 : 220;
+        $("body").animate({scrollTop: s}, 300);
+    });
+</script>
 @endsection
